57ce77d43cff2de48a60b49b2b689d69ac123436
Miroslaw Chmielecki <misiak19@op.pl>
2019-10-28 15:52:21 +0100

    change path DB in config.js

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: 8000,
   DB:
-    "mongodb+srv://Miro19ak:<Mirek19ak>@cluster0-0all7.mongodb.net/test?retryWrites=true&w=majority"
+    "mongodb+srv://Miro19ak:Mirek19ak@cluster0-0all7.mongodb.net/test?retryWrites=true&w=majority"
 };
