57494d0d42982ce29f2cd1bd235e6c6263f44069
Miroslaw Chmielecki <misiak19@op.pl>
2019-10-29 14:40:45 +0100

    add vars in heroku

--- server/config.js
+++ server/config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  PORT: process.env.PORT || 8000,
-  DB:
-    "mongodb+srv://Miro19ak:Mirek19ak@cluster0-0all7.mongodb.net/test?retryWrites=true&w=majority"
+  PORT: process.env.PORT,
+  DB: process.env.DB
 };
