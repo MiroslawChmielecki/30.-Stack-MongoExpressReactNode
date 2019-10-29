f0b416b7b180feb8023a2ab149723efdf996cdf2
Miroslaw Chmielecki <misiak19@op.pl>
2019-10-29 11:13:15 +0100

    remove build from gitignore

---
+++ client/build/precache-manifest.70c317bfa54538a15690f42655ac5c84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76299b6db6fdd8ce24d2024c20d5e7c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "459b6400ba7d174fd21a",
+    "url": "/static/css/2.ee498ddd.chunk.css"
+  },
+  {
+    "revision": "06724131262386c97c5c",
+    "url": "/static/css/main.090eb977.chunk.css"
+  },
+  {
+    "revision": "459b6400ba7d174fd21a",
+    "url": "/static/js/2.000c1e0a.chunk.js"
+  },
+  {
+    "revision": "06724131262386c97c5c",
+    "url": "/static/js/main.676b4e1f.chunk.js"
+  },
+  {
+    "revision": "96d9506c0ec10f86d92f",
+    "url": "/static/js/runtime-main.a245edba.js"
+  }
+]);
